50551e82dd65053b51a1bedf0dec6fe0ef867ed3
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-08-04 09:03:54 +0100

    Mail functionality and Procfile added

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,5 +1,6 @@
 const Ticket = require('../models/ticket')
 const qrCode = require('qrcode')
+const nodemailer = require('nodemailer')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
@@ -16,14 +17,44 @@ const createTicket = async (req, res) => {
                             error: err
                         })
                     } else {
-                        res.status(200).json({
-                            message: "Ticket created successfully",
-                            url: url
+                        
+                        const Name = process.env.NAME
+                        const transporter = nodemailer.createTransport({
+                            host: 'smtp.zoho.eu',
+                            port: 465,
+                            auth: {
+                                user: process.env.MAIL,
+                                pass: process.env.PASS
+                            }
                         })
+                        const mailOptions = {
+                            from: process.env.MAIL,
+                            to: email,
+                            subject: 'Ticket Confirmation',
+                            text: `Hello ${name},\n\nYour ticket has been created successfully.\n\n${url} \n \n Have a safe Journey! \n ${Name}`
+                        }
+                        transporter.sendMail(mailOptions, (err) => {
+                            if (err) {
+                                res.status(500).json({
+                                    message: "Error in sending email",
+                                    error: err
+                                })
+                            } else {
+                                res.status(200).json({
+                                    message: "Email sent successfully"
+                                })
+                            }
+                        })
+
                     }
+                    // res.status(200).json({
+                    //     message: "Ticket created successfully",
+                    //     url: url
+                    // })
                 }
             )
         })
+
 }
 
 const getTickets = async (req, res) => {
@@ -84,59 +115,59 @@ const updateTicket = async (req, res) => {
             message: 'User not found'
         })
     } else {
-            const { id } = req.params
-            const { name, email, phone, address, vehicle, from, to, date, time, next_of_kin_name, next_of_kin_phone, next_of_kin_email, next_of_kin_address, next_of_kin_relationship } = req.body
-            try {
-                const ticket = await Ticket.findByIdAndUpdate(id, {
-                    name,
-                    email,
-                    phone,
-                    address,
-                    vehicle,
-                    from,
-                    to,
-                    date,
-                    time,
-                    next_of_kin_name,
-                    next_of_kin_phone,
-                    next_of_kin_email,
-                    next_of_kin_address,
-                    next_of_kin_relationship
-                }, { new: true })
-                res.status(200).json({
-                    message: 'Ticket updated successfully',
-                    ticket
-                })
-            } catch (err) {
-                res.status(500).json({
-                    message: err.message
-                })
-            }
-        }
-}
-    const deleteTicket = async (req, res) => {
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const email = decoded.email
-        const user = User.findByEmail(email)
-        if (!user) {
-            res.status(404).json({
-                message: 'User not found'
+        const { id } = req.params
+        const { name, email, phone, address, vehicle, from, to, date, time, next_of_kin_name, next_of_kin_phone, next_of_kin_email, next_of_kin_address, next_of_kin_relationship } = req.body
+        try {
+            const ticket = await Ticket.findByIdAndUpdate(id, {
+                name,
+                email,
+                phone,
+                address,
+                vehicle,
+                from,
+                to,
+                date,
+                time,
+                next_of_kin_name,
+                next_of_kin_phone,
+                next_of_kin_email,
+                next_of_kin_address,
+                next_of_kin_relationship
+            }, { new: true })
+            res.status(200).json({
+                message: 'Ticket updated successfully',
+                ticket
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message
             })
         }
-        else {
-            const { id } = req.params
-            try {
-                const ticket = await Ticket.findByIdAndDelete(id)
-                res.status(200).json({
-                    message: 'Ticket deleted successfully',
-                    ticket
-                })
-            } catch (err) {
-                res.status(500).json({
-                    message: err.message
-                })
-            }
+    }
+}
+const deleteTicket = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const email = decoded.email
+    const user = User.findByEmail(email)
+    if (!user) {
+        res.status(404).json({
+            message: 'User not found'
+        })
+    }
+    else {
+        const { id } = req.params
+        try {
+            const ticket = await Ticket.findByIdAndDelete(id)
+            res.status(200).json({
+                message: 'Ticket deleted successfully',
+                ticket
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message
+            })
         }
     }
-    module.exports = { createTicket, getTickets, getTicket, updateTicket, deleteTicket }
+}
+module.exports = { createTicket, getTickets, getTicket, updateTicket, deleteTicket }
