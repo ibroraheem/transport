e764afacdced473c9b677bc2281969677729372c
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-08-04 19:50:02 +0100

    routes refactor

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const {createTicket, getTickets, getTicket, updateTicket, deleteTicket} = require('../controllers/ticketController')
 
 router.post('/create', createTicket)
-router.get('/', getTickets)
-router.get('/:id', getTicket)
-router.delete('/:id', deleteTicket)
-router.patch('/:id', updateTicket)
+router.get('/tickets', getTickets)
+router.get('/ticket/:id', getTicket)
+router.delete('/ticket/:id', deleteTicket)
+router.patch('/ticket/:id', updateTicket)
 
 module.exports = router
